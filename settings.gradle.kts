44ea8c5179ec511e837cce2471e9ab8689421a00
entrezunfredici <superfred2468@gmail.com>
2023-11-19 16:20:24 +0100

    Add empty mobile app

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Peojet Jeux d'echec Mobile Macabiau Frederic"
+include(":app")
+ 
